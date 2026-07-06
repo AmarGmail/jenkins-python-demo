a0a520b09f21bf17bd876dd7110786bdd082e001
Amarjyoti Lahkar <amarjyotilahkar37@gmail.com>
2026-07-06 05:04:08 +0000

    feat: added circle area

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import math
+
 def add(a,b):
     return a + b
 def area_ractangle(x, y):
     return x * y
 
+def circle_area(r):
+    area = (math.pi * (r ** 2))
+    return round(area,3)
+#    return area
+
 if __name__ == "__main__":
     print("Hello from jenkins")
     print("Sum is: ", add(4,5))
     print("Area is: ", area_ractangle(4, 5) )
+    print("Circle area is: ", circle_area(5.5) )
